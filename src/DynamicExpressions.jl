0f3aec600edc5f571e93c1feff4b1d5bc88116fa
MilesCranmer <miles.cranmer@gmail.com>
2022-10-16 15:04:19 -0400

    Export `NodeIndex`

--- src/DynamicExpressions.jl
+++ src/DynamicExpressions.jl
@@ -12,7 +12,7 @@ include("OperatorEnumConstruction.jl")
 
 using Reexport
 @reexport import .EquationModule: Node, string_tree
-@reexport import .EquationUtilsModule: count_nodes
+@reexport import .EquationUtilsModule: count_nodes, NodeIndex
 @reexport import .OperatorEnumConstructionModule: OperatorEnum
 @reexport import .EvaluateEquationModule: eval_tree_array, differentiable_eval_tree_array
 @reexport import .EvaluateEquationDerivativeModule: eval_diff_tree_array, eval_grad_tree_array
