c618acebdb8c30f6a85b24fca215060ac1e02952
MilesCranmer <miles.cranmer@gmail.com>
2022-10-16 14:10:45 -0400

    Import all files to `DynamicExpressions.jl`

--- src/DynamicExpressions.jl
+++ src/DynamicExpressions.jl
@@ -1,3 +1,12 @@
 module DynamicExpressions
 
+include("Utils.jl")
+include("OperatorEnum.jl")
+include("Equation.jl")
+include("EquationUtils.jl")
+include("EvaluateEquation.jl")
+include("EvaluateEquationDerivative.jl")
+include("InterfaceSymbolicUtils.jl")
+include("SimplifyEquation.jl")
+
 end
