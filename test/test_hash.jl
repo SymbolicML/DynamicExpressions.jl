586cc6186c9fb60af8592eafd8b87091f0645490
MilesCranmer <miles.cranmer@gmail.com>
2022-10-16 14:42:23 -0400

    Add missing function to hash test

--- test/test_hash.jl
+++ test/test_hash.jl
@@ -1,6 +1,10 @@
 using DynamicExpressions
 using Test
 
+function greater(x::T, y::T) where T
+    (x > y) ? one(T) : zero(T)
+end
+
 operators = OperatorEnum(; binary_operators=(+, *, ^, /, greater), unary_operators=(cos,))
 tree = Node(3, (Node(; val=3.0) * Node(1, Node("x1")))^2.0, Node(; val=-1.2))
 x = hash(tree)
--- test/test_params.jl
+++ test/test_params.jl
@@ -3,4 +3,7 @@ using DynamicExpressions
 maximum_residual = 1e-2
 custom_cos(x) = cos(x)
 
-operators = OperatorEnum(; binary_operators=(/, +, *), unary_operators=(exp, custom_cos))
+default_params = (
+    binary_operators=(/, +, *),
+    unary_operators=(exp, custom_cos)
+)
