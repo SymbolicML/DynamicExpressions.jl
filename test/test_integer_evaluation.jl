8bbcd5c83819929bfd37a72ac856fca193d87017
MilesCranmer <miles.cranmer@gmail.com>
2022-10-16 15:20:54 -0400

    Fix integer evaluation test

--- test/test_integer_evaluation.jl
+++ test/test_integer_evaluation.jl
@@ -5,13 +5,13 @@ include("test_params.jl")
 
 # Test evaluation on integer-based trees.
 operators = OperatorEnum(;
-    default_params..., binary_operators=(+, *, /, -), unary_operators=(square,)
+    default_params..., binary_operators=(+, *, /, -, ^), unary_operators=()
 )
 
-nodefnc(x1, x2, x3) = x2 * x3 + Int32(2) - square(x1)
+nodefnc(x1, x2, x3) = x2 * x3 + Int32(2) - x1 ^ Int32(2)
 
-x1, x2, x3 = Node("x1"), Node("x2"), Node("x3")
-tree = nodefnc(x1, x2, x3)
+xs = [Node(Int32; feature=i) for i in 1:3]
+tree = nodefnc(xs...)
 
 tree = convert(Node{Int32}, tree)
 X = Int32.(rand(MersenneTwister(0), -5:5, 3, 100))
--- test/test_params.jl
+++ test/test_params.jl
@@ -10,8 +10,6 @@ safe_pow(x::T, y::T) where T = (x < 0 && y != round(y)) ? T(NaN) : x ^ y
 relu(x::T) where T = (x < 0) ? zero(T) : x
 safe_acosh(x::T) where T = (x < 1) ? T(NaN) : acosh(x)
 sub(x::T, y::T) where T = x - y
-square(x::T) where T = x^2
-cube(x::T) where T = x^3
 greater(x::T, y::T) where T = (x > y) ? one(T) : zero(T)
 
 safe_log(x) = log(x)
@@ -23,8 +21,8 @@ safe_pow(x, y) = x ^ y
 relu(x) = max(x, 0)
 safe_acosh(x) = acosh(x)
 sub(x, y) = x - y
-square(x) = x^2
-cube(x) = x^3
+square(x) = x * x
+cube(x) = x * x * x
 greater(x, y) = (x > y)
 
 custom_cos(x) = cos(x)
