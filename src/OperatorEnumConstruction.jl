d63a797e0a76156db8f545a0b42b107fff45a072
MilesCranmer <miles.cranmer@gmail.com>
2022-10-16 19:00:53 -0400

    Construct ease-of-use functions for evaluation

--- src/OperatorEnumConstruction.jl
+++ src/OperatorEnumConstruction.jl
@@ -3,6 +3,7 @@ module OperatorEnumConstructionModule
 import Zygote: gradient
 import ..OperatorEnumModule: OperatorEnum
 import ..EquationModule: string_tree, Node
+import ..EvaluateEquationModule: eval_tree_array
 
 function OperatorEnum(;
     binary_operators=[], unary_operators=[], enable_autodiff::Bool=false
@@ -112,6 +113,22 @@ function OperatorEnum(;
     @eval begin
         Base.print(io::IO, tree::Node) = print(io, string_tree(tree, $operators))
         Base.show(io::IO, tree::Node) = print(io, string_tree(tree, $operators))
+
+        function (tree::Node{T})(X::AbstractArray{T,2})::AbstractArray{T,1} where {T<:Real}
+            out, did_finish = eval_tree_array(tree, X, $operators)
+            if !did_finish
+                out .= T(NaN)
+            end
+            return out
+        end
+        function (tree::Node{T1})(X::AbstractArray{T2,2}) where {T1<:Real,T2<:Real}
+            if T1 != T2
+                T = promote_type(T1, T2)
+                tree = convert(Node{T}, tree)
+                X = T.(X)
+            end
+            return tree(X)
+        end
     end
 
     return operators
