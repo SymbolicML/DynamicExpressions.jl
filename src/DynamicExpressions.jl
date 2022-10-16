77727dc40197a7313f08cfd9b2f7dd2f41c3aad5
MilesCranmer <miles.cranmer@gmail.com>
2022-10-16 14:56:58 -0400

    Export other needed functions

--- src/DynamicExpressions.jl
+++ src/DynamicExpressions.jl
@@ -14,7 +14,7 @@ using Reexport
 @reexport import .EquationModule: Node, string_tree
 @reexport import .EquationUtilsModule: count_nodes
 @reexport import .OperatorEnumConstructionModule: OperatorEnum
-@reexport import .EvaluateEquationModule: eval_tree_array
+@reexport import .EvaluateEquationModule: eval_tree_array, differentiable_eval_tree_array
 @reexport import .EvaluateEquationDerivativeModule: eval_diff_tree_array, eval_grad_tree_array
 @reexport import .InterfaceSymbolicUtilsModule: node_to_symbolic, symbolic_to_node
 
