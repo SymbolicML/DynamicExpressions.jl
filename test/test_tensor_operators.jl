89a81bfb6ed10b7976f42d0105a83f638023a1f7
MilesCranmer <miles.cranmer@gmail.com>
2022-10-19 14:40:50 -0400

    Fix formatting issue

--- test/test_tensor_operators.jl
+++ test/test_tensor_operators.jl
@@ -27,7 +27,9 @@ function vec_square(x)
     return x .* x
 end
 
-operators = GenericOperatorEnum(; binary_operators=[vec_add], unary_operators=[vec_square], extend_user_operators=true)
+operators = GenericOperatorEnum(;
+    binary_operators=[vec_add], unary_operators=[vec_square], extend_user_operators=true
+)
 tree = Node(1, c1)
 @test repr(tree) == "vec_square([1.0, 2.0, 3.0])"
 @test tree(X) == [1.0, 4.0, 9.0]
