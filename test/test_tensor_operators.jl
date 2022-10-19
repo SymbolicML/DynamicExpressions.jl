1581a51a7bfe5a3b3d34fa39cc7bed95d45f254d
MilesCranmer <miles.cranmer@gmail.com>
2022-10-19 14:00:11 -0400

    Include test for unary operator

--- test/test_tensor_operators.jl
+++ test/test_tensor_operators.jl
@@ -21,3 +21,16 @@ tree = Node(1, c1, x2)
 tree = Node(1, x1, c1)
 @test repr(tree) == "vec_add(x1, [1.0, 2.0, 3.0])"
 @test tree(X) == [3.0, 4.0, 5.0]
+
+# Also test unary operators:
+function vec_square(x)
+    return x .* x
+end
+
+operators = GenericOperatorEnum(; binary_operators=[vec_add], unary_operators=[vec_square], extend_user_operators=true)
+tree = Node(1, c1)
+@test repr(tree) == "vec_square([1.0, 2.0, 3.0])"
+@test tree(X) == [1.0, 4.0, 9.0]
+tree = Node(1, Node(1, c1), x1)
+@test repr(tree) == "vec_add(vec_square([1.0, 2.0, 3.0]), x1)"
+@test tree(X) == [3.0, 6.0, 11.0]
