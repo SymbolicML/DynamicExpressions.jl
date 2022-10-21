8702a47c47c232506bcf15ebf44509e1f66814ee
MilesCranmer <miles.cranmer@gmail.com>
2022-10-20 21:14:45 -0400

    Fix method error

--- test/test_error_handling.jl
+++ test/test_error_handling.jl
@@ -2,13 +2,20 @@ using DynamicExpressions
 using Test
 
 # Test that we generate errors:
+baseT = Float64
+T = Union{baseT,Vector{baseT},Matrix{baseT}}
+
 scalar_add(x::T, y::T) where {T<:Real} = x + y
+
 operators = GenericOperatorEnum(; binary_operators=[scalar_add], extend_user_operators=true)
-tree = scalar_add(x1, x2)
+
+x1, x2, x3 = [Node(T; feature=i) for i in 1:3]
+
+tree = Node(1, x1, x2)
 
 # With error handling:
 try
-    eval_tree_array(tree, [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], operators; catch_errors=true)
+    eval_tree_array(tree, [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], operators; throw_errors=true)
     @test false
 catch e
     @test isa(e, ErrorException)
@@ -18,7 +25,7 @@ end
 
 # Without error handling:
 output, flag = eval_tree_array(
-    tree, [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], operators; catch_errors=false
+    tree, [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], operators; throw_errors=false
 )
 @test output === nothing
 @test !flag
@@ -32,4 +39,4 @@ catch e
 end
 
 # But can be overrided:
-output = tree([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]; catch_errors=false)
+output = tree([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]; throw_errors=false)
