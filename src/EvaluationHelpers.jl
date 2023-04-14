628b103ba42f4611ae6162b9c43b33c40fff571d
MilesCranmer <miles.cranmer@gmail.com>
2023-04-14 16:02:20 -0400

    Allow `tree'(X, operators; variable=false)`

--- src/EvaluationHelpers.jl
+++ src/EvaluationHelpers.jl
@@ -61,8 +61,8 @@ function (tree::Node)(X; kws...)
 end
 
 # Gradients:
-function _grad_evaluator(tree::Node, X, operators::OperatorEnum; kws...)
-    _, grad, did_complete = eval_grad_tree_array(tree, X, operators; variable=true, kws...)
+function _grad_evaluator(tree::Node, X, operators::OperatorEnum; variable=true, kws...)
+    _, grad, did_complete = eval_grad_tree_array(tree, X, operators; variable=variable, kws...)
     !did_complete && (grad .= convert(eltype(grad), NaN))
     return grad
 end
