c330340b70663cd1550089afdec67dbc89ba3c32
MilesCranmer <miles.cranmer@gmail.com>
2023-04-27 16:56:31 -0400

    Fix error in is_constant

--- src/EquationUtils.jl
+++ src/EquationUtils.jl
@@ -51,7 +51,7 @@ has_operators(tree::Node) = tree.degree > 0
 Check if an expression is a constant numerical value, or
 whether it depends on input features.
 """
-is_constant(tree::Node) = all(t -> t.degree > 0 || tree.constant, tree)
+is_constant(tree::Node) = all(t -> t.degree > 0 || t.constant, tree)
 
 """
     get_constants(tree::Node{T})::Vector{T} where {T}
