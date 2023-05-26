42311a60050a943e86fce84bbcb98c13851beae6
Miles Cranmer <miles.cranmer@gmail.com>
2023-05-26 14:38:50 -0400

    Fix error message

--- src/ExtensionInterface.jl
+++ src/ExtensionInterface.jl
@@ -11,7 +11,7 @@ function node_to_symbolic(args...; kws...)
 end
 function symbolic_to_node(args...; kws...)
     return error(
-        "Please load the `SymbolicUtils` package to use `symbolic_to_node(::Symbolic, ::AbstractOperatorEnum, kws...)`.",
+        "Please load the `SymbolicUtils` package to use `symbolic_to_node(::Symbolic, ::AbstractOperatorEnum; kws...)`.",
     )
 end
 
