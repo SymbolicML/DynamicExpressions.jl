7532e21f7ba2ddce6712aa977de8adf69b9034ba
MilesCranmer <miles.cranmer@gmail.com>
2023-05-26 11:13:18 -0400

    Make SymbolicUtils.jl a weak dependency

--- src/DynamicExpressions.jl
+++ src/DynamicExpressions.jl
@@ -7,10 +7,10 @@ include("EquationUtils.jl")
 include("EvaluateEquation.jl")
 include("EvaluateEquationDerivative.jl")
 include("EvaluationHelpers.jl")
-include("InterfaceSymbolicUtils.jl")
 include("SimplifyEquation.jl")
 include("OperatorEnumConstruction.jl")
 
+using Requires
 using Reexport
 @reexport import .EquationModule:
     Node, string_tree, print_tree, copy_node, set_node!, tree_mapreduce, filter_map
@@ -30,9 +30,15 @@ using Reexport
 @reexport import .EvaluateEquationModule: eval_tree_array, differentiable_eval_tree_array
 @reexport import .EvaluateEquationDerivativeModule:
     eval_diff_tree_array, eval_grad_tree_array
-@reexport import .InterfaceSymbolicUtilsModule: node_to_symbolic, symbolic_to_node
 @reexport import .SimplifyEquationModule: combine_operators, simplify_tree
 @reexport import .EvaluationHelpersModule
+
+function __init__()
+    @require SymbolicUtils = "d1185830-fcd6-423d-90d6-eec64667417b" @eval begin
+        include("InterfaceSymbolicUtils.jl")
+        @reexport import .InterfaceSymbolicUtilsModule: node_to_symbolic, symbolic_to_node
+    end
+end
 
 include("deprecated.jl")
 
