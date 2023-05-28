f74cbafa0f2855e87b6b2eb8e9d1a7d2703ff16a
MilesCranmer <miles.cranmer@gmail.com>
2023-05-27 20:28:05 -0400

    Fix

--- src/OperatorEnumConstruction.jl
+++ src/OperatorEnumConstruction.jl
@@ -1,22 +1,16 @@
 module OperatorEnumConstructionModule
 
+import Requires: @init, @require
 import ..OperatorEnumModule: AbstractOperatorEnum, OperatorEnum, GenericOperatorEnum
 import ..EquationModule: string_tree, Node
 import ..EvaluateEquationModule: eval_tree_array
 import ..EvaluateEquationDerivativeModule: eval_grad_tree_array
 import ..EvaluationHelpersModule: _grad_evaluator
 
-const ZygoteLoaded = Ref(false)
-const ZygoteLock = Threads.SpinLock()
-function load_zygote()
-    ZygoteLoaded.x && return nothing
-    lock(ZygoteLock) do
-        ZygoteLoaded.x && return nothing
-        @eval import Zygote: gradient
-        ZygoteLoaded.x = true
-        return nothing
-    end
-end
+#! format: off
+generate_diff_operators(::Any, ::Any) = error("`Zygote` not loaded.")
+@init @require Zygote = "e88e6eb3-aa80-5325-afca-941959d7151f" include("zygote_interface.jl")
+#! format: on
 
 function create_evaluation_helpers!(operators::OperatorEnum)
     @eval begin
@@ -230,7 +224,8 @@ function OperatorEnum(;
     unary_operators = Function[op for op in unary_operators]
 
     diff_bin, diff_una = if enable_autodiff
-        generate_diff_operators(binary_operators, unary_operators)
+        Base.require(@__MODULE__, :Zygote)
+        Base.invokelatest(generate_diff_operators, binary_operators, unary_operators)
     else
         Function[], Function[]
     end
@@ -243,21 +238,6 @@ function OperatorEnum(;
     end
 
     return operators
-end
-
-function generate_diff_operators(binary_operators, unary_operators)
-    load_zygote()
-    diff_bin = Function[]
-    diff_una = Function[]
-    for op in binary_operators
-        diff_op(x, y) = gradient(op, x, y)
-        push!(diff_bin, diff_op)
-    end
-    for op in unary_operators
-        diff_op(x) = gradient(op, x)[1]
-        push!(diff_una, diff_op)
-    end
-    return diff_bin, diff_una
 end
 
 """
---
+++ src/zygote_interface.jl
@@ -0,0 +1,21 @@
+import Zygote: gradient
+
+make_diff_bin(op) = (x, y) -> gradient(op, x, y)
+make_diff_una(op) = x -> gradient(op, x)[1]
+
+function generate_diff_operators(
+    binary_operators::Vector{Function}, unary_operators::Vector{Function}
+)
+    diff_bin = Function[]
+    diff_una = Function[]
+
+    for op in binary_operators
+        diff_op = make_diff_bin(op)
+        push!(diff_bin, diff_op)
+    end
+    for op in unary_operators
+        diff_op = make_diff_una(op)
+        push!(diff_una, diff_op)
+    end
+    return diff_bin, diff_una
+end
