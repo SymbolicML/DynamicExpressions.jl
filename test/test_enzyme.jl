9d195e5ee34c30b178cefa5ee50d21daf82198db
MilesCranmer <miles.cranmer@gmail.com>
2024-01-14 01:51:50 +0000

    Add known broken enzyme test

--- test/test_enzyme.jl
+++ test/test_enzyme.jl
@@ -35,3 +35,36 @@ autodiff(
 true_dX = cat(ones(100), -sin.(X[2, :]), zeros(100); dims=2)'
 
 @test true_dX ≈ dX
+
+# Broken test (see https://github.com/EnzymeAD/Enzyme.jl/issues/1241)
+function my_loss_function(tree, X, operators)
+    # Get the outputs
+    y = tree(X, operators)
+    # Sum them (so we can take a gradient, rather than a jacobian)
+    return sum(y)
+end
+tree = 0.5 * x1 + cos(x2 - 0.2)
+
+# Just to keep things simple:
+X = [1.0 1.0;]
+
+d_tree = begin
+    storage_tree = copy(tree)
+    # Set all constants to zero:
+    foreach(storage_tree) do node
+        if node.degree == 0 && node.constant
+            node.val = 0.0
+        end
+    end
+    autodiff(
+        Reverse,
+        my_loss_function,
+        Active,
+        Duplicated(tree, storage_tree),
+        Const(X),
+        Const(operators),
+    )
+    storage_tree
+end
+
+@test_broken get_constants(d_tree) ≈ [1.0, 0.717356]
