a968ac17e1a38a59f8481ac1cb5acc190cb606c6
MilesCranmer <miles.cranmer@gmail.com>
2024-01-25 11:50:38 +0900

    Add Optim to tests

--- ext/DynamicExpressionsOptimExt.jl
+++ ext/DynamicExpressionsOptimExt.jl
@@ -1,14 +1,14 @@
 module DynamicExpressionsOptimExt
 
-using DynamicExpressions: Node, eval_tree_array
+using DynamicExpressions: AbstractExpressionNode, eval_tree_array
+using Compat: @inline
 
-import Compat: @inline
 import Optim: optimize
 
 """Wrap f with insertion of values of the constant nodes."""
 function get_wrapped_f(
     f::F, tree::N, constant_nodes::AbstractArray{N}
-) where {F,T,N<:Node{T}}
+) where {F,T,N<:AbstractExpressionNode{T}}
     function wrapped_f(x)
         for (ci, xi) in zip(constant_nodes, x)
             ci.val::T = xi::T
@@ -21,7 +21,7 @@ end
 """Wrap g! or h! with insertion of values of the constant nodes."""
 function get_wrapped_gh!(
     gh!::GH, tree::N, constant_nodes::AbstractArray{N}
-) where {GH,T,N<:Node{T}}
+) where {GH,T,N<:AbstractExpressionNode{T}}
     function wrapped_gh!(G, x)
         for (ci, xi) in zip(constant_nodes, x)
             ci.val::T = xi::T
@@ -32,9 +32,9 @@ function get_wrapped_gh!(
     return wrapped_gh!
 end
 
-function optimize(f::F, g!::G, h!::H, tree::Node{T}, args...; kwargs...) where {F,G,H,T}
+function optimize(f::F, g!::G, h!::H, tree::AbstractExpressionNode{T}, args...; kwargs...) where {F,G,H,T}
     constant_nodes = filter(t -> t.degree == 0 && t.constant, tree)
-    x0 = [t.val::T for t in constant_nodes]
+    x0 = T[t.val::T for t in constant_nodes]
     if g! === nothing
         @assert h! === nothing
         return optimize(get_wrapped_f(f, tree, constant_nodes), x0, args...; kwargs...)
@@ -57,10 +57,10 @@ function optimize(f::F, g!::G, h!::H, tree::Node{T}, args...; kwargs...) where {
         )
     end
 end
-function optimize(f::F, g!::G, tree::Node, args...; kwargs...) where {F,G}
+function optimize(f::F, g!::G, tree::AbstractExpressionNode, args...; kwargs...) where {F,G}
     return optimize(f, g!, nothing, tree, args...; kwargs...)
 end
-function optimize(f::F, tree::Node, args...; kwargs...) where {F}
+function optimize(f::F, tree::AbstractExpressionNode, args...; kwargs...) where {F}
     return optimize(f, nothing, tree, args...; kwargs...)
 end
 
