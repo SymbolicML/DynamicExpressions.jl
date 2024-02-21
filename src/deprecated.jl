ea7c03d72101d39ed9f44356a217cd13b3a8b827
MilesCranmer <miles.cranmer@gmail.com>
2024-02-21 20:22:40 +0000

    Generalize deprecated constructors

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,42 +1,71 @@
 import Base: @deprecate
-import .EquationModule: Node
+import .EquationModule: Node, GraphNode
 
 @deprecate set_constants set_constants!
 @deprecate simplify_tree simplify_tree!
 
-function Node(d::Integer, c::Bool, v::T) where {T}
-    Base.depwarn("Node(d, c, v) is deprecated. Use Node{T}(val=v) instead.", :Node)
-    @assert d == 1
-    @assert c == true
-    return Node{T}(; val=v)
-end
-function Node(::Type{T}, d::Integer, c::Bool, v::_T) where {T,_T}
-    Base.depwarn("Node(T, d, c, v) is deprecated. Use Node{T}(val=v) instead.", :Node)
-    @assert d == 1
-    @assert c == true
-    return Node{T}(; val=v)
-end
-function Node(::Type{T}, d::Integer, c::Bool, ::Nothing, f::Integer) where {T}
-    Base.depwarn(
-        "Node(T, d, c, v, f) is deprecated. Use Node{T}(feature=f) instead.", :Node
-    )
-    @assert d == 1
-    @assert c == false
-    return Node{T}(; feature=f)
-end
-function Node(d::Integer, ::Bool, ::Nothing, ::Integer, o::Integer, l::Node)
-    Base.depwarn(
-        "Node(d, c, v, f, o, l) is deprecated. Use Node(operator=o, left=l) instead.", :Node
-    )
-    @assert d == 1
-    return Node(; op=o, l=l)
-end
-function Node(
-    d::Integer, ::Bool, ::Nothing, ::Integer, o::Integer, l::Node{T}, r::Node{T}
-) where {T}
-    Base.depwarn(
-        "Node(d, c, v, f, o, l, r) is deprecated. Use Node(op=o, l=l, r=r) instead.", :Node
-    )
-    @assert d == 2
-    return Node(; op=o, l=l, r=r)
+for N in (:Node, :GraphNode)
+    @eval begin
+        function $N(d::Integer, c::Bool, v::T) where {T}
+            Base.depwarn(
+                string($N) *
+                "(d, c, v) is deprecated. Use " *
+                string($(N)) *
+                "{T}(val=v) instead.",
+                $(Meta.quot(N)),
+            )
+            @assert d == 1
+            @assert c == true
+            return $N{T}(; val=v)
+        end
+        function $N(::Type{T}, d::Integer, c::Bool, v::_T) where {T,_T}
+            Base.depwarn(
+                string($N) *
+                "(T, d, c, v) is deprecated. Use " *
+                string($(N)) *
+                "{T}(val=v) instead.",
+                $(Meta.quot(N)),
+            )
+            @assert d == 1
+            @assert c == true
+            return $N{T}(; val=v)
+        end
+        function $N(::Type{T}, d::Integer, c::Bool, ::Nothing, f::Integer) where {T}
+            Base.depwarn(
+                string($N) *
+                "(T, d, c, v, f) is deprecated. Use " *
+                string($(N)) *
+                "{T}(feature=f) instead.",
+                $(Meta.quot(N)),
+            )
+
+            @assert d == 1
+            @assert c == false
+            return $N{T}(; feature=f)
+        end
+        function $N(d::Integer, ::Bool, ::Nothing, ::Integer, o::Integer, l::$N)
+            Base.depwarn(
+                string($N) *
+                "(d, c, v, f, o, l) is deprecated. Use " *
+                string($(N)) *
+                "(op=o, l=l) instead.",
+                $(Meta.quot(N)),
+            )
+            @assert d == 1
+            return $N(; op=o, l=l)
+        end
+        function $N(
+            d::Integer, ::Bool, ::Nothing, ::Integer, o::Integer, l::$N{T}, r::$N{T}
+        ) where {T}
+            Base.depwarn(
+                string($N) *
+                "(d, c, v, f, o, l, r) is deprecated. Use " *
+                string($(N)) *
+                "(op=o, l=l, r=r) instead.",
+                $(Meta.quot(N)),
+            )
+            @assert d == 2
+            return $N(; op=o, l=l, r=r)
+        end
+    end
 end
