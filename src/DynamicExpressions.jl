93574a3a5faae71d388cb036f66d2728cf21e83c
MilesCranmer <miles.cranmer@gmail.com>
2024-02-26 01:15:34 +0000

    Start switching to KernelAbstractions.jl

--- src/DynamicExpressions.jl
+++ src/DynamicExpressions.jl
@@ -88,7 +88,8 @@ import .EvaluateModule: ArrayBuffer, ResultOk
 @reexport import .ChainRulesModule: NodeTangent, extract_gradient
 @reexport import .SimplifyModule: combine_operators, simplify_tree!
 @reexport import .EvaluationHelpersModule
-@reexport import .ExtensionInterfaceModule: node_to_symbolic, symbolic_to_node
+@reexport import .ExtensionInterfaceModule:
+    node_to_symbolic, symbolic_to_node, gpu_eval_tree_array
 @reexport import .RandomModule: NodeSampler
 @reexport import .ExpressionModule:
     AbstractExpression,
