68cf0a0c07bcb4c28871f4ecd04a27d26a77b92b
MilesCranmer <miles.cranmer@gmail.com>
2024-03-09 18:33:35 +0000

    Turn back on unbound args check

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,4 @@
 using DynamicExpressions
 using Aqua
 
-Aqua.test_all(DynamicExpressions; project_toml_formatting=false, unbound_args=false)
+Aqua.test_all(DynamicExpressions; project_toml_formatting=false)
