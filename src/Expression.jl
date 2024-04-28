32d2d07bcfb438030b389a5ad5c830f199e878a8
MilesCranmer <miles.cranmer@gmail.com>
2024-04-28 18:27:41 +0100

    docs: for `Expression` and `@parse_expression`

--- src/Expression.jl
+++ src/Expression.jl
@@ -31,6 +31,28 @@ function options(::AbstractExpression)
     )
 end
 
+"""
+    Expression{T, N, O, V} <: AbstractExpression{T, N}
+
+Defines a high level, user-facing, expression type that encapsulates an
+expression tree (like `Node`) along with associated metadata for evaluation and rendering.
+
+# Fields
+
+- `tree::N`: The root node of the raw expression tree.
+- `operators::O`: Operators indexed by the expression tree.
+- `variable_names::V`: A vector of variable names used when printing the expression.
+
+# Constructors
+
+- Expression(tree::AbstractExpressionNode, operators::AbstractOperatorEnum, variable_names::AbstractVector): Construct from the fields
+- @parse_expression(expr, operators=operators, variable_names=variable_names, node_type=Node): Parse a Julia expression with a given context and create an Expression object.
+
+# Usage
+
+This type is intended for end-users to interact with and manipulate expressions at a high level,
+abstracting away the complexities of the underlying expression tree operations.
+"""
 struct Expression{
     T,
     N<:AbstractExpressionNode{T},
--- src/Parse.jl
+++ src/Parse.jl
@@ -4,6 +4,58 @@ using ..NodeModule: AbstractExpressionNode, Node
 using ..OperatorEnumModule: AbstractOperatorEnum
 using ..ExpressionModule: AbstractExpression, Expression
 
+"""
+    @parse_expression(expr; operators, variable_names, node_type=Node)
+
+Parse a symbolic expression `expr` into a computational graph where nodes represent operations or variables.
+
+## Arguments
+- `expr`: An expression to parse into an `AbstractExpression`.
+- `operators`: An instance of `OperatorEnum` specifying the available unary and binary operators.
+- `variable_names`: A list of variable names as strings or symbols that are allowed in the expression.
+
+## Keyword Arguments
+- `node_type`: The type of the nodes in the resulting expression tree. Defaults to `Node`.
+
+## Usage
+The macro is used to convert a high-level symbolic expression into a structured expression tree that can be manipulated or evaluated. Here are some examples of how to use `parse_expression`:
+
+- Parsing a simple mathematical expression involving custom operations:
+
+```julia
+julia> my_custom_op(x, y) = x + y^3;
+
+julia> operators = OperatorEnum(binary_operators=[+, -, *, my_custom_op], unary_operators=[sin]);
+
+julia> ex = @parse_expression my_custom_op(x, sin(y) + 0.3) operators=operators variable_names=["x", "y"]
+my_custom_op(x, sin(y) + 0.3)
+
+julia> typeof(ex)
+Expression{Float64, Node{Float64}, OperatorEnum{Tuple{typeof(+), typeof(-), typeof(*), typeof(my_custom_op)}, Tuple{typeof(sin)}}, Vector{String}}
+
+julia> typeof(ex.tree)
+Node{Float64}
+
+julia> ex(ones(2, 1))
+1-element Vector{Float64}:
+ 2.487286478935302
+```
+
+- Handling expressions with symbolic variable names, and custom node types:
+
+```julia
+julia> ex = @parse_expression(
+            cos(exp(α - 1)),
+            operators=OperatorEnum(binary_operators=[-], unary_operators=[cos, exp]),
+            variable_names=[:α],
+            node_type=GraphNode
+        )
+cos(exp(α))
+
+julia> typeof(ex.tree)
+GraphNode{Float32}
+```
+"""
 macro parse_expression(ex, kws...)
     # Initialize default values for operators and variable_names
     operators = nothing
