518d3ed655b276542b05f2cc64b6c58593be87b1
MilesCranmer <miles.cranmer@gmail.com>
2024-05-08 00:31:01 +0100

    test: avoid redefining MultiScalarExpression

--- test/test_multi_expression.jl
+++ test/test_multi_expression.jl
@@ -5,10 +5,7 @@ using DynamicExpressions
 using DynamicExpressions: DynamicExpressions as DE
 using DynamicExpressions: Metadata
 
-@gensym MultiScalarExpression
-
-@eval struct $(MultiScalarExpression){T,TREES<:NamedTuple,D<:NamedTuple} <:
-             AbstractExpression{T}
+struct MultiScalarExpression{T,TREES<:NamedTuple,D<:NamedTuple} <: AbstractExpression{T}
     trees::TREES
     metadata::Metadata{D}
 
@@ -18,7 +15,7 @@ using DynamicExpressions: Metadata
     The `tree_factory` is a function that takes the trees by keyword argument,
     and stitches them together into a single tree (for printing or evaluation).
     """
-    function $(MultiScalarExpression)(
+    function MultiScalarExpression(
         trees::NamedTuple; tree_factory::F, operators, variable_names
     ) where {F<:Function}
         T = eltype(first(values(trees)))
@@ -34,7 +31,7 @@ variable_names = ["a", "b", "c"]
 ex1 = @parse_expression(c * 2.5 - cos(a), operators, variable_names)
 ex2 = @parse_expression(b * b * b + c / 0.2, operators, variable_names)
 
-multi_ex = @eval $(MultiScalarExpression)(
+multi_ex = MultiScalarExpression(
     (; f=ex1.tree, g=ex2.tree);
     tree_factory=(; f, g) -> Node(; op=1, l=f, r=g),
     # TODO: Can we build the tree factory from another expression maybe?
@@ -58,14 +55,14 @@ if VERSION >= v"1.9"
 end
 
 tree_factory(f::F, trees) where {F} = f(; trees...)
-@eval function DE.get_tree(ex::$(MultiScalarExpression))
+function DE.get_tree(ex::MultiScalarExpression)
     # `tree_factory` should stitch the nodes together
     return tree_factory(ex.metadata.tree_factory, ex.trees)
 end
-@eval function DE.get_operators(ex::$(MultiScalarExpression), operators)
+function DE.get_operators(ex::MultiScalarExpression, operators)
     return operators === nothing ? ex.metadata.operators : operators
 end
-@eval function DE.get_variable_names(ex::$(MultiScalarExpression), variable_names)
+function DE.get_variable_names(ex::MultiScalarExpression, variable_names)
     return variable_names === nothing ? ex.metadata.variable_names : variable_names
 end
 
