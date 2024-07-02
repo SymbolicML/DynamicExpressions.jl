3aae22d4e767750eeb5c610545b635f0ca1ffd2f
MilesCranmer <miles.cranmer@gmail.com>
2024-07-02 20:14:43 +0100

    docs: add redirect page for `types` page

--- docs/make.jl
+++ docs/make.jl
@@ -11,4 +11,34 @@ makedocs(;
     warnonly=true,
 )
 
+# Forward links from old docs:
+
+redirect_page = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+    <script type="text/javascript">
+        var fragment = window.location.hash;
+        window.location.href = "../api/" + fragment;
+    </script>
+</head>
+<body>
+    <p>If you are not redirected automatically, follow this <a id="redirect-link" href="../api/">link to API</a>.</p>
+    <script type="text/javascript">
+        document.getElementById('redirect-link').href = "../api/" + window.location.hash;
+    </script>
+</body>
+</html>
+"""
+
+# Create the types directory and write the redirect page
+types_dir = joinpath("docs", "build", "types")
+mkpath(types_dir)
+redirect_file = joinpath(types_dir, "index.html")
+open(redirect_file, "w") do f
+    write(f, redirect_page)
+end
+
 deploydocs(; repo="github.com/SymbolicML/DynamicExpressions.jl.git")
