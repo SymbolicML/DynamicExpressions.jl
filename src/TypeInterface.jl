bdf4ad008ea4f097077de7f50080800000431157
gca30 <gca30@cam.ac.uk>
2024-07-05 15:27:35 +0100

    type interface suggestions

--- src/TypeInterface.jl
+++ src/TypeInterface.jl
@@ -4,13 +4,15 @@ module TypeInterfaceModule
 
 # Is a value of your type valid?
 is_valid(x::T) where {T} = true #=::Bool=#
+@inline is_valid(x::T) where {T<:Number} = isfinite(x) && !isnan(x)
+
+# Is an array of values of your type valid?
 is_valid_array(x::AbstractArray{T}) where {T} = all(is_valid, x) #=::Bool=#
+@inline is_valid_array(x::AbstractArray{T}) where {T<:Number} = is_valid(sum(x)) # Fastest way to check for NaN in an array.
 
 # Obtain the underlying number type
-function get_number_type(::Type{T}) where {T} #=::DataType=#
-    error("Base number type of your custom type is not defined")
-    return T
-end
+get_number_type(::Type{T}) where {T} = error("Base number type of your custom type is not defined")  #=::DataType=#
+get_number_type(::Type{W}) where {W<:Number} = W
 
 """
     append_number_constants!(nvals, idx, value)
@@ -19,8 +21,9 @@ Fill all constants stored in your `value` to `nvals`, starting from `idx`. Retur
 """
 function append_number_constants!(
     nvals::AbstractVector{BT}, idx::Int64, value::T
-) where {BT<:Number,T}
-    return idx
+) where {BT<:Number,T<:Number} #= ::Int64 =#
+    nvals[idx] = value
+    return idx + 1
 end
 
 """
@@ -32,12 +35,12 @@ fill `value` with `nvals` starting from `idx`.
 Returns a tuple of the next index to read from, and the filled-in value.
 
 !!! note
-    In the case of a non-`Number` `value`, this will simply return `(idx, value)` without affecting `nvals` as default behaviour.
+    In the case of a `Number` `value`, this will simply return `(idx, value)` without affecting `nvals` as default behaviour.
 """
 function pop_number_constants(
     nvals::AbstractVector{BT}, idx::Int64, value::T
-) where {T,BT<:Number}
-    return (idx, value)
+) where {T,BT<:Number} #= ::Tuple{Int64, T}  =#
+    return (idx+1, convert(T, nvals[idx]))
 end
 
 """
@@ -45,24 +48,6 @@ Count how many scalar constants `value` has, for use in `append_number_constants
 
 Note that this will return 0 as default behaviour (meaning your type has no scalars to be optimized).
 """
-count_number_constants(value::T) where {T} = 0
-
-# Implementations for numbers:
-
-get_number_type(::Type{W}) where {W<:Number} = W
-function append_number_constants!(
-    nvals::AbstractVector{BT}, idx::Int64, value::T
-) where {BT<:Number,T<:Number}
-    nvals[idx] = value
-    return idx + 1
-end
-function pop_number_constants(
-    nvals::AbstractVector{BT}, idx::Int64, value::T
-) where {T<:Number,BT<:Number}
-    return idx + 1, nvals[idx]
-end
 @inline count_number_constants(::T) where {T<:Number} = 1
-@inline is_valid(x::T) where {T<:Number} = isfinite(x) && !isnan(x)
-@inline is_valid_array(x::AbstractArray{T}) where {T<:Number} = is_valid(sum(x)) # Fastest way to check for NaN in an array.
 
 end
