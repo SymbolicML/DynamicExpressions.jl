ee7d7c1ce2731f2d15eb00b4836a3d67bdbf4d04
MilesCranmer <miles.cranmer@gmail.com>
2024-07-19 22:13:23 +0100

    style: rename to `eval_options`

--- ext/DynamicExpressionsBumperExt.jl
+++ ext/DynamicExpressionsBumperExt.jl
@@ -12,7 +12,7 @@ function bumper_eval_tree_array(
     tree::AbstractExpressionNode{T},
     cX::AbstractMatrix{T},
     operators::OperatorEnum,
-    options::EvaluationOptions{turbo,true,early_exit},
+    eval_options::EvaluationOptions{turbo,true,early_exit},
 ) where {T,turbo,early_exit}
     result = similar(cX, axes(cX, 2))
     n = size(cX, 2)
@@ -38,7 +38,7 @@ function bumper_eval_tree_array(
             # In the evaluation kernel, we combine the branch nodes
             # with the arrays created by the leaf nodes:
             ((args::Vararg{Any,M}) where {M}) ->
-                dispatch_kerns!(operators, args..., options),
+                dispatch_kerns!(operators, args..., eval_options),
             tree;
             break_sharing=Val(true),
         )
@@ -50,68 +50,67 @@ function bumper_eval_tree_array(
 end
 
 function dispatch_kerns!(
-    operators, branch_node, cumulator, options::EvaluationOptions{turbo,true,early_exit}
-) where {turbo,early_exit}
+    operators,
+    branch_node,
+    cumulator,
+    eval_options::EvaluationOptions{<:Any,true,early_exit},
+) where {early_exit}
     cumulator.ok || return cumulator
 
-    out = dispatch_kern1!(operators.unaops, branch_node.op, cumulator.x, options)
-    return early_exit ? ResultOk(out, is_valid_array(out)) : ResultOk(out, true)
+    out = dispatch_kern1!(operators.unaops, branch_node.op, cumulator.x, eval_options)
+    return ResultOk(out, early_exit ? is_valid_array(out) : true)
 end
 function dispatch_kerns!(
     operators,
     branch_node,
     cumulator1,
     cumulator2,
-    options::EvaluationOptions{turbo,true,early_exit},
-) where {turbo,early_exit}
+    eval_options::EvaluationOptions{<:Any,true,early_exit},
+) where {early_exit}
     cumulator1.ok || return cumulator1
     cumulator2.ok || return cumulator2
 
     out = dispatch_kern2!(
-        operators.binops, branch_node.op, cumulator1.x, cumulator2.x, options
+        operators.binops, branch_node.op, cumulator1.x, cumulator2.x, eval_options
     )
-    return early_exit ? ResultOk(out, is_valid_array(out)) : ResultOk(out, true)
+    return ResultOk(out, early_exit ? is_valid_array(out) : true)
 end
 
 @generated function dispatch_kern1!(
-    unaops, op_idx, cumulator, options::EvaluationOptions{turbo,true,early_exit}
-) where {turbo,early_exit}
+    unaops, op_idx, cumulator, eval_options::EvaluationOptions
+)
     nuna = counttuple(unaops)
     quote
         Base.@nif(
-            $nuna, i -> i == op_idx, i -> let op = unaops[i]
-                return bumper_kern1!(op, cumulator, options)
+            $nuna,
+            i -> i == op_idx,
+            i -> let op = unaops[i]
+                return bumper_kern1!(op, cumulator, eval_options)
             end,
         )
     end
 end
 @generated function dispatch_kern2!(
-    binops,
-    op_idx,
-    cumulator1,
-    cumulator2,
-    options::EvaluationOptions{turbo,true,early_exit},
-) where {turbo,early_exit}
+    binops, op_idx, cumulator1, cumulator2, eval_options::EvaluationOptions
+)
     nbin = counttuple(binops)
     quote
         Base.@nif(
             $nbin,
             i -> i == op_idx,
             i -> let op = binops[i]
-                return bumper_kern2!(op, cumulator1, cumulator2, options)
+                return bumper_kern2!(op, cumulator1, cumulator2, eval_options)
             end,
         )
     end
 end
-function bumper_kern1!(
-    op::F, cumulator, ::EvaluationOptions{false,true,early_exit}
-) where {F,early_exit}
+function bumper_kern1!(op::F, cumulator, ::EvaluationOptions{false,true}) where {F}
     @. cumulator = op(cumulator)
     return cumulator
 end
 function bumper_kern2!(
-    op::F, cumulator1, cumulator2, ::EvaluationOptions{false,true,early_exit}
-) where {F,early_exit}
+    op::F, cumulator1, cumulator2, ::EvaluationOptions{false,true}
+) where {F}
     @. cumulator1 = op(cumulator1, cumulator2)
     return cumulator1
 end
