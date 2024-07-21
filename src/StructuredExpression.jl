ef518b3b3a9861bd0c1ebe3eaf264a27bff84c32
MilesCranmer <miles.cranmer@gmail.com>
2024-07-21 23:59:35 +0100

    docs: for `StructuredExpression`

--- src/StructuredExpression.jl
+++ src/StructuredExpression.jl
@@ -17,12 +17,46 @@ import ..ExpressionModule:
     get_constants,
     set_constants!
 
+"""
+    StructuredExpression
+
+This expression type allows you to combine multiple expressions
+together in a predefined way.
+
+For example, we can create two expressions, `f`, and `g`,
+and then combine them together in a new expression, `f_plus_g`,
+using a constructor function that simply adds them together:
+
+```julia
+f = parse_expression(:(x * x - cos(2.5f0 * y + -0.5f0)); kws...)
+g = parse_expression(:(exp(-(y * y))); kws...)
+
+f_plus_g = StructuredExpression((; f, g), nt -> nt.f + nt.g)
+```
+
+Now, when evaluating `f_plus_g`, this expression type will
+return the result of adding together the results of `f` and `g`.
+
+You can dispatch on a particular structured expression with the second
+type parameter, `F`, which is the function defined above:
+
+```julia
+my_factory(nt) = nt.f + nt.g
+
+Base.show(io::IO, e::StructuredExpression{T,typeof(my_factory)}) where {T} = ...
+```
+
+which will create a new method particular to this expression type defined on that function.
+"""
 struct StructuredExpression{
     T,
+    F,
     N<:AbstractExpressionNode{T},
     E<:AbstractExpression{T,N},
     TS<:NamedTuple{<:Any,<:NTuple{<:Any,E}},
-    D<:NamedTuple{(:structure, :operators, :variable_names)},
+    O,
+    V,
+    D<:NamedTuple{structure::F,operators::O,variable_names::V},
 } <: AbstractExpression{T,N}
     trees::TS
     metadata::Metadata{D}
