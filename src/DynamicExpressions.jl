8a6b7c435002577cc20c12b36ea1439434869b08
MilesCranmer <miles.cranmer@gmail.com>
2024-07-22 06:12:23 +0100

    test: add Literate.jl example with StructuredExpression

--- src/DynamicExpressions.jl
+++ src/DynamicExpressions.jl
@@ -78,9 +78,15 @@ import .NodeModule:
 @reexport import .ExtensionInterfaceModule: node_to_symbolic, symbolic_to_node
 @reexport import .RandomModule: NodeSampler
 @reexport import .ExpressionModule:
-    AbstractExpression, Expression, with_contents, with_metadata, get_contents, get_metadata
+    AbstractExpression,
+    Expression,
+    with_contents,
+    with_metadata,
+    get_contents,
+    get_metadata,
+    get_tree
 import .ExpressionModule:
-    get_tree, get_operators, get_variable_names, Metadata, default_node_type, node_type
+    get_operators, get_variable_names, Metadata, default_node_type, node_type
 @reexport import .ExpressionAlgebraModule: @declare_expression_operator
 @reexport import .ParseModule: @parse_expression, parse_expression
 import .ParseModule: parse_leaf
--- test/test_structured_expression.jl
+++ test/test_structured_expression.jl
@@ -77,3 +77,55 @@ end
 
     @test h(ex) == 2
 end
+
+@testitem "StructuredExpression Literate examples" begin
+    #literate_begin file="src/structured_expression.md"
+    #=
+    `StructuredExpression`s allow you to specify a predefined structure for an
+    expression that exists outside of the regular `AbstractExpressionNode` objects
+    which store expressions as trees.
+
+    Let's look at an example:
+    =#
+    using DynamicExpressions
+    using Test
+
+    # First, we will create some normal `Expression` objects.
+
+    operators = OperatorEnum(; unary_operators=(cos, exp), binary_operators=(+, -, *, /))
+    variable_names = ["x", "y"]
+    x = Expression(Node{Float64}(; feature=1); operators, variable_names)
+    y = Expression(Node{Float64}(; feature=2); operators, variable_names)
+    @test typeof(x) <: AbstractExpression{Float64,<:Node{Float64}}
+
+    typeof(x)
+    #=
+    `AbstractExpression` can be composed together using standard Julia
+    operations. For example, let's create two complex expressions from these,
+    and check that they have the same type:
+    =#
+    f = x * x - cos(2.5f0 * y + -0.5f0)
+    g = exp(2.0 - y * y)
+
+    @test typeof(f) == typeof(g) == typeof(x)
+    f, g
+    #=
+    We can then create a `StructuredExpression` from these two expressions.
+    This is a composite `AbstractExpression` object that composes multiple
+    expressions during evaluation.
+    =#
+    ex = StructuredExpression((; f, g), nt -> nt.f + nt.g)
+    ex
+    #=
+    Note that this is displayed as a single tree, with the `+` operator
+    used to combine them. Despite this, the expression is not actually
+    *stored* with the `+` operator in an `AbstractExpressionNode`.
+
+    By default, using `get_tree` will evaluate the result of `nt.f + nt.g`.
+    This let's us use things like the regular operations available to
+    `AbstractExpressionNode`s:
+    =#
+    length(get_tree(ex))
+
+    #literate_end
+end
