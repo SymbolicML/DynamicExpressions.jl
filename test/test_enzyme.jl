0a2bb96415d6efa0f5173aa715715d9cb090afd7
Niklas Heim <heim.niklas@gmail.com>
2024-07-24 14:59:50 +0200

    fix enzyme test

--- test/test_enzyme.jl
+++ test/test_enzyme.jl
@@ -5,9 +5,6 @@ using DynamicExpressions
 operators = OperatorEnum(; binary_operators=(+, -, *, /), unary_operators=(cos, sin))
 # TODO: More operators will trigger a segfault in Enzyme
 
-# These options are required for Enzyme to work:
-const eval_options = (turbo=Val(false),)
-
 x1, x2, x3 = (i -> Node(Float64; feature=i)).(1:3)
 
 tree = Node(1, x1, Node(1, x2))  # == x1 + cos(x2)
@@ -16,7 +13,7 @@ X = randn(3, 100);
 dX = zero(X)
 
 function f(tree, X, operators, output)
-    output[] = sum(eval_tree_array(tree, X, operators; eval_options...)[1])
+    output[] = sum(eval_tree_array(tree, X, operators)[1])
     return nothing
 end
 
