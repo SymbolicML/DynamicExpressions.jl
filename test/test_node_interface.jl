1d5ba269fe23ca5c06fef354e4acd2c1f0756ea7
MilesCranmer <miles.cranmer@gmail.com>
2024-07-27 23:17:52 +0100

    test: more coverage of interfaces

--- test/test_node_interface.jl
+++ test/test_node_interface.jl
@@ -10,17 +10,26 @@
 
     tree_branch_deg2 = x1 + sin(x2 * 3.5)
     tree_branch_deg1 = sin(x1)
-    tree_leaf = x1
+    tree_leaf_feature = x1
+    tree_leaf_constant = Node{Float64}(; val=1.0)
     graph_tree_branch_deg2 = convert(GraphNode, tree_branch_deg2)
     graph_tree_branch_deg1 = convert(GraphNode, tree_branch_deg1)
-    graph_tree_leaf = convert(GraphNode, tree_leaf)
+    graph_tree_leaf_feature = convert(GraphNode, tree_leaf_feature)
+    graph_tree_leaf_constant = convert(GraphNode, tree_leaf_constant)
 
     @test Interfaces.test(
-        NodeInterface, Node, [tree_branch_deg2, tree_branch_deg1, tree_leaf]
+        NodeInterface,
+        Node,
+        [tree_branch_deg2, tree_branch_deg1, tree_leaf_feature, tree_leaf_constant],
     )
     @test Interfaces.test(
         NodeInterface,
         GraphNode,
-        [graph_tree_branch_deg2, graph_tree_branch_deg1, graph_tree_leaf],
+        [
+            graph_tree_branch_deg2,
+            graph_tree_branch_deg1,
+            graph_tree_leaf_feature,
+            graph_tree_leaf_constant,
+        ],
     )
 end
