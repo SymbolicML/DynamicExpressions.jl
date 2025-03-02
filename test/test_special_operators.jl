30f163e777c3b01e042031d80fe246f47098f804
MilesCranmer <miles.cranmer@gmail.com>
2025-03-02 01:05:13 +0000

    test: clean up

--- test/test_special_operators.jl
+++ test/test_special_operators.jl
@@ -8,9 +8,9 @@ using TestItems: @testitem
     using Random
 
     # Define operators and variable names
-    assign_op2 = AssignOperator(; target_register=2)
+    assign_x2 = AssignOperator(; target_register=2)
     operators = OperatorEnum(;
-        binary_operators=[+, -, *, /], unary_operators=[sin, cos, assign_op2]
+        binary_operators=[+, -, *, /], unary_operators=[sin, cos, assign_x2]
     )
     variable_names = ["x1", "x2", "x3", "x4", "x5"]
 
@@ -23,7 +23,7 @@ using TestItems: @testitem
     #    and then add the return to `x2` (which should now be 3.0!)
     x1 = Expression(Node(; feature=1); operators, variable_names)
     x2 = Expression(Node(; feature=2); operators, variable_names)
-    assign_expr = assign_op2(0.0 * x1 + 3.0) + x2
+    assign_expr = assign_x2(0.0 * x1 + 3.0) + x2
 
     @test string_tree(assign_expr) == "[x2 =]((0.0 * x1) + 3.0) + x2"
 
@@ -36,7 +36,7 @@ using TestItems: @testitem
     @test X[2, :] == [0.5, 1.5, 2.5]
 
     # But, with the reverse order, we get the x2 _before_ it was reassigned
-    assign_expr_reverse = x2 + assign_op2(0.0 * x1 + 3.0)
+    assign_expr_reverse = x2 + assign_x2(0.0 * x1 + 3.0)
     result, completed = eval_tree_array(assign_expr_reverse, X)
     @test completed == true
     @test result == [3.5, 4.5, 5.5]
