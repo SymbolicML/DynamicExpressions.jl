7b04a1530e97d112500557dab0dc86de19d7619f
MilesCranmer <miles.cranmer@gmail.com>
2025-05-11 20:50:16 +0100

    refactor: remove reference stuff from read only nodes

--- src/ReadOnlyNode.jl
+++ src/ReadOnlyNode.jl
@@ -2,38 +2,24 @@ module ReadOnlyNodeModule
 
 using DispatchDoctor: @unstable
 
-using ..NodeModule: AbstractExpressionNode, Node
+using ..NodeModule: AbstractExpressionNode, Node, max_degree
 import ..NodeModule: default_allocator, with_type_parameters, constructorof, get_children
 
-abstract type AbstractReadOnlyNode{T,D,N<:AbstractExpressionNode{T,D},IS_REF} <:
+abstract type AbstractReadOnlyNode{T,D,N<:AbstractExpressionNode{T,D}} <:
               AbstractExpressionNode{T,D} end
 
 """A type of expression node that prevents writing to the inner node"""
-struct ReadOnlyNode{T,D,N,IS_REF} <: AbstractReadOnlyNode{T,D,N,IS_REF}
+struct ReadOnlyNode{T,D,N} <: AbstractReadOnlyNode{T,D,N}
     _inner::N
 
-    function ReadOnlyNode(
-        n::N, ::Val{IS_REF}
-    ) where {T,D,N<:AbstractExpressionNode{T,D},IS_REF}
-        return new{T,D,N,IS_REF}(n)
-    end
-    function ReadOnlyNode(n::N) where {T,D,N<:AbstractExpressionNode{T,D}}
-        return ReadOnlyNode(n, Val(false))
-    end
-    function ReadOnlyNode(n::AbstractReadOnlyNode)
-        return n
-    end
-    function ReadOnlyNode(n::Ref{<:AbstractExpressionNode})
-        return ReadOnlyNode(n[], Val(true))
-    end
+    ReadOnlyNode(n::N) where {T,N<:AbstractExpressionNode{T}} = new{T,max_degree(N),N}(n)
 end
 @inline inner(n::AbstractReadOnlyNode) = getfield(n, :_inner)
 @unstable constructorof(::Type{<:ReadOnlyNode}) = ReadOnlyNode
-Base.getindex(n::AbstractReadOnlyNode{T,D,N,true} where {T,D,N}) = n
 @inline function Base.getproperty(n::AbstractReadOnlyNode, s::Symbol)
     out = getproperty(inner(n), s)
-    if out isa Union{AbstractExpressionNode,Ref{<:AbstractExpressionNode}}
-        return ReadOnlyNode(out)
+    if out isa AbstractExpressionNode
+        return constructorof(typeof(n))(out)
     else
         return out
     end
@@ -44,7 +30,7 @@ end
 function Base.setproperty!(::AbstractReadOnlyNode, ::Symbol, v)
     return error("Cannot set properties on a ReadOnlyNode")
 end
-Base.propertynames(n::AbstractReadOnlyNode) = propertynames(getfield(n, :_inner))
-Base.copy(n::AbstractReadOnlyNode) = ReadOnlyNode(copy(getfield(n, :_inner)))
+Base.propertynames(n::AbstractReadOnlyNode) = propertynames(inner(n))
+Base.copy(n::AbstractReadOnlyNode) = ReadOnlyNode(copy(inner(n)))
 
 end
