7d2a542baa24c1f4ccaf257d8799d24ca0b3d971
MilesCranmer <miles.cranmer@gmail.com>
2025-05-31 00:33:32 +0100

    test: incorporate turbo and bumper in supposition test

--- test/test_supposition_consistency.jl
+++ test/test_supposition_consistency.jl
@@ -7,6 +7,8 @@
     using DynamicExpressions
     using DynamicExpressions:
         string_tree, parse_expression, eval_tree_array, Node, get_operators, get_tree
+    using LoopVectorization: LoopVectorization
+    using Bumper: Bumper
 
     # bring the generator into scope
     include("supposition_utils.jl")
@@ -20,7 +22,7 @@
         T; num_features=n_features, max_layers=max_layers, operators=operators
     )
 
-    @check function roundtrip_string(ex=expr_gen)
+    result = @check function roundtrip_string(ex=expr_gen)
         tree_str = string_tree(ex)
         ex_parsed = parse_expression(
             Meta.parse(tree_str);
@@ -30,15 +32,25 @@
         )
         return ex == ex_parsed
     end
+    @test something(result.result) isa Supposition.Pass
 
     input_gen = make_input_matrix_generator(T; n_features)
-    @check max_examples = 1024 function eval_against_string(ex=expr_gen, X=input_gen)
-        expression_result, ok = eval_tree_array(ex, X)
-        !ok && return true  # If the expression is not valid, we can't test it
+    args_gen = map(
+        (ex, X, turbo, bumper) -> (; ex, X, turbo, bumper),
+        expr_gen,
+        input_gen,
+        Data.Booleans(),
+        Data.Booleans(),
+    )
+    # We only consider expressions that don't have NaN/Inf/etc.
+    clean_args_gen = filter(args -> eval_tree_array(args.ex, args.X)[2], args_gen)
+    result2 = @check max_examples = 1000 function eval_against_string(args=clean_args_gen)
+        (; ex, X, turbo, bumper) = args
+        expression_result, ok = eval_tree_array(ex, X; turbo, bumper)
         tree_str = string_tree(ex)
-        f_sym = gensym("f")
         f = eval(Meta.parse("(x1, x2, x3, x4, x5) -> ($tree_str)"))
         true_result = Float64[Base.invokelatest(f, x...) for x in eachcol(X)]
-        return expression_result ≈ true_result
+        return ok && expression_result ≈ true_result
     end
+    @test something(result2.result) isa Supposition.Pass
 end
