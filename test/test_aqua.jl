753d740a87a4e950267a913ef6c24f702c8113ba
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-01-31 10:13:15 +0000

    revert(tests): drop Aqua shim and pin Aqua compat

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,14 +1,4 @@
 using DynamicExpressions
 using Aqua
 
-# `project_toml_formatting` keyword was added in newer Aqua versions.
-# Keep compatibility with older Aqua while allowing newer versions to disable formatting checks.
-try
-    Aqua.test_all(DynamicExpressions; project_toml_formatting=false)
-catch err
-    if err isa MethodError && occursin("project_toml_formatting", sprint(showerror, err))
-        Aqua.test_all(DynamicExpressions)
-    else
-        rethrow()
-    end
-end
+Aqua.test_all(DynamicExpressions; project_toml_formatting=false)
