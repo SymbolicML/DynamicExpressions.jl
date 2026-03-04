741031c44ec84e44b2a53a3caba5bc8d50ba2bbf
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-03-04 14:11:45 +0000

    fix: treat im as imaginary unit in string parsing
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- src/Parse.jl
+++ src/Parse.jl
@@ -200,6 +200,21 @@ end
     )
 end
 
+@unstable @inline _replace_imaginary_unit_symbol(ex) = ex
+@unstable @inline _replace_imaginary_unit_symbol(ex::Symbol) = ex === :im ? im : ex
+@unstable @inline function _replace_imaginary_unit_symbol(ex::Expr)
+    return Expr(ex.head, map(_replace_imaginary_unit_symbol, ex.args)...)
+end
+
+@unstable @inline function _normalize_expression_for_parse(
+    ex, variable_names::Union{AbstractVector{<:AbstractString},Nothing}
+)
+    if variable_names !== nothing && ("im" in variable_names)
+        return ex
+    end
+    return _replace_imaginary_unit_symbol(ex)
+end
+
 """Parse an expression Julia `Expr` object."""
 @unstable function parse_expression(
     ex;
@@ -237,6 +252,7 @@ end
             operators
         end
 
+        ex = _normalize_expression_for_parse(ex, variable_names)
         tree = _parse_expression(ex, operators, variable_names, N, E, evaluate_on; kws...)
         return constructorof(E)(tree; operators, variable_names, kws...)
     end
