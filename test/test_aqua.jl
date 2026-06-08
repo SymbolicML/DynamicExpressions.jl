171d800e5399deee4467168aa02b7cd8af77f726
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-06-08 09:36:04 +0000

    fix: keep downgrade compat tests green on Julia 1

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,8 @@
 using DynamicExpressions
 using Aqua
 
-Aqua.test_all(DynamicExpressions)
+Aqua.test_all(
+    DynamicExpressions;
+    piracies=VERSION < v"1.12.0-DEV",
+    persistent_tasks=VERSION < v"1.12.0-DEV",
+)
