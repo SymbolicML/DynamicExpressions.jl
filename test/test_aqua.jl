8556b1adefda2bf53376c6615271b74f18e4b24f
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-06-11 19:50:23 +0000

    fix: restore default Aqua checks
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,8 +1,4 @@
 using DynamicExpressions
 using Aqua
 
-Aqua.test_all(
-    DynamicExpressions;
-    piracies=VERSION < v"1.12.0-DEV",
-    persistent_tasks=VERSION < v"1.12.0-DEV",
-)
+Aqua.test_all(DynamicExpressions)
