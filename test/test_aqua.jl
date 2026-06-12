30e2d76160e82834fc2cd79a6476a0ee20c993aa
MilesCranmer <miles.cranmer@gmail.com>
2026-06-12 12:39:53 +0100

    test: revert Aqua workaround (belongs in a separate PR)

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,10 +1,4 @@
 using DynamicExpressions
 using Aqua
 
-const aqua_08_on_julia_112 = Base.pkgversion(Aqua) == v"0.8.0" && VERSION >= v"1.12"
-
-Aqua.test_all(
-    DynamicExpressions;
-    piracies=!aqua_08_on_julia_112,
-    persistent_tasks=!aqua_08_on_julia_112,
-)
+Aqua.test_all(DynamicExpressions)
