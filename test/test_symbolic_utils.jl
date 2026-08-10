97652e179ed75c5820e6ffb339ab496c16b9087f
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-08-10 23:50:39 +0000

    test: allow Symbolics operand reordering
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- test/test_symbolic_utils.jl
+++ test/test_symbolic_utils.jl
@@ -35,7 +35,7 @@ a, b = Symbolics.@variables a b
 symbolics_tree = symbolic_to_node(
     a + b, OperatorEnum(; binary_operators=(+,)); variable_names=["a", "b"]
 )
-@test string(symbolics_tree) == "x1 + x2"
+@test string(symbolics_tree) in ("x1 + x2", "x2 + x1")
 
 # Test variable name conversion with Expression objects
 let
