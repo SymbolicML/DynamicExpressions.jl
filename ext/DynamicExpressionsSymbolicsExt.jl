527258eda8b70556afb61f3b0a5eacd8aac06b53
MilesCranmerBot <258324893+MilesCranmerBot@users.noreply.github.com>
2026-08-10 15:51:51 +0000

    fix: keep Symbolics conversion in SymbolicUtils extension
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- ext/DynamicExpressionsSymbolicsExt.jl
+++
@@ -1,9 +0,0 @@
-module DynamicExpressionsSymbolicsExt
-
-using Symbolics: Num, value
-
-import DynamicExpressions.ExtensionInterfaceModule: _symbolic_to_node
-
-_symbolic_to_node(expr::Num, args...; kws...) = _symbolic_to_node(value(expr), args...; kws...)
-
-end
